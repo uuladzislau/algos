28744908b07f3a11bc016e4c31ab649efd85ea03
Uladzislau Ulasovich <ulasowitch.vlad@gmail.com>
2024-06-11 21:51:16 +0200

    Add solution for '94. Binary Tree Inorder Traversal'

---
+++ src/94_bitree_inorder_traversal.js
@@ -0,0 +1,13 @@
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  if (!root) return [];
+
+  const left = inorderTraversal(root.left);
+
+  const right = inorderTraversal(root.right);
+
+  return [...left, root.val, ...right];
+};
