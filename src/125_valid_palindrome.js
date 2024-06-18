197afe9933396012d7ff59ced42a28de4fcaa8cd
Uladzislau Ulasovich <ulasowitch.vlad@gmail.com>
2024-06-18 18:25:24 +0200

    Update solution for '125. Valid Palindrome'

--- src/125_valid_palindrome.js
+++ src/125_valid_palindrome.js
@@ -4,49 +4,33 @@
  */
 var isPalindrome = function(s) {
 
-  // A = 65
-  // Z = 90
-  // a = 97
-  // z = 122
-  // 0 = 48
-  // 9 = 57
-
+  // 0 = 48, 9 = 57
   const isNumber = (charCode) => 48 <= charCode && charCode <= 57;
 
+  // a = 97, z = 122
   const isLowerAlpha = (charCode) => 65 <= charCode && charCode <= 90;
 
+  // A = 65, Z = 90
   const isUpperAlpha = (charCode) => 97 <= charCode && charCode <= 122;
 
-  const isAlphanumeric = (charCode) => isNumber(charCode) || isLowerAlpha(charCode) || isUpperAlpha(charCode);
+  for (let left = 0, right = s.length - 1; left < right;) {
+    const leftCharCode = s.charCodeAt(left);
+    const rightCharCode = s.charCodeAt(right);
 
-  const equalIgnoreCase = (leftCharCode, rightCharCode) => {
-    if (isNumber(leftCharCode) && isNumber(rightCharCode)) return leftCharCode === rightCharCode;
+    if (!(isNumber(leftCharCode) || isLowerAlpha(leftCharCode) || isUpperAlpha(leftCharCode))) {
+      left++;
+      continue;
+    }
+
+    if (!(isNumber(rightCharCode) || isLowerAlpha(rightCharCode) || isUpperAlpha(rightCharCode))) {
+      right--;
+      continue;
+    }
 
     const normalizedLeft = isLowerAlpha(leftCharCode) ? leftCharCode : leftCharCode - 32;
     const normalizedRight = isLowerAlpha(rightCharCode) ? rightCharCode : rightCharCode - 32;
 
-    return normalizedLeft === normalizedRight;
-  };
-
-  let left = 0;
-  let right = s.length - 1;
-
-  while (right - left > 0) {
-
-    let leftCharCode = s.charCodeAt(left);
-    let rightCharCode = s.charCodeAt(right);
-
-    while (!isAlphanumeric(leftCharCode) && left <= s.length) {
-      leftCharCode = s.charCodeAt(++left);
-    }
-
-    while (!isAlphanumeric(rightCharCode) && right >= 0) {
-      rightCharCode = s.charCodeAt(--right);
-    }
-
-    if (!leftCharCode && !rightCharCode) return true;
-
-    if (!equalIgnoreCase(leftCharCode, rightCharCode)) {
+    if (normalizedLeft !== normalizedRight) {
       return false;
     }
 
